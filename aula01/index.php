8b3776a4e7a3de7229864dfaa893ca728e354014
Rafael Sancor <rafaelsancor2003@hotmail.com>
2025-02-26 10:43:19 -0300

    Create index.php

---
+++ aula01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Principal</title>
+</head>
+<body>
+    <h1>Página principal</h1>
+    
+    <?php
+    echo "Hello World!";
+    ?> 
+</body>
+</html>
