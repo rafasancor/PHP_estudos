6894db71ee31ba4a67331e60ff658a8200d5b9da
Rafael Sancor <rafaelsancor2003@hotmail.com>
2025-05-14 19:44:58 -0300

    Create menu.php

---
+++ aula11/menu.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="clientes.php">Clientes Cadastrados</a>
+        </li>
+    </ul>
+</nav>
